8145bfb2985790d67b486ff3bb33443c0c4700a4
Amoo Abeeb <amooabeeb0@gmail.com>
2016-11-28 11:12:59 +0100

    worked on the staff page and the css

---
+++ about.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Central Science Laboratory</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />
+    <link rel="stylesheet" href="css/style.css" type="text/css" />
+    <script  src="css/jquery.js"></script>
+    <script  src="css/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+<?php include ('includes/header.inc'); ?><br/>
+<div class="container-fluid about_container">
+    <div class="about_main">
+        <h3 class="text-center" style="font-family:"Helvetica Neue", cursive, Helvetica, Arial, sans-serif"><b>ABOUT US</b></h3><br/>
+        <img src="images/logo.png" style="float:left" class="img-responsive" />
+        <p>
+            The availability of well-equipped and functional laboratories for multi-disciplinary research purposes is
+            condition sine qua non for scientific development of developing nations, Nigeria inclusive.
+        </p>
+        <p>
+            The University Central Science Laboratory (CSL) was established in 1999 as a cost-optimizing innovation for
+            providing modern expensive facilities, centrally, for qualitative instruction at both the undergraduate and
+            postgraduate levels to enhance the quality of research in a wide range of science disciplines. Hitherto,
+            individual departments procured instruments/equipment leading to duplication of expensive facilities with
+            no spare parts to service them and researchers had no idea what equipment/facilities existed in other
+            departments/units. The CSL was thus established to address theseproblems by pooling the resources under
+            one management and placing them at the disposal of both staff and students.
+        </p>
+        <p>
+            Facilities that are considered expensive but of priority to primary academic programmes of multidisciplinary
+            application and require support of infrastructure are selected for management by the CSL in a purpose-built
+            CSL building,backed up with electricity generators of 114 and 22.5KVA respectively in the event of power
+            failure.
+        </p>
+        <p>
+            <b>The CSL is categorized into five sections according to spatial location of items of equipment viz:</b>
+            <ul>
+                <li>Biology Laboratory</li>
+                <li>Chemistry Laboratory</li>
+                <li>Nuclear Magnetic Resonance (NMR) Laboratory</li>
+                <li>Microscopy laboratory including Scanning Electron microscope (SEM)</li>
+                <li>Biotechnology Laboratory</li>
+            </ul>
+        </p>
+        <p>
+            The management of the laboratory is under a Director with Lab conveners/Coordinators in charge of the
+            various sections listed above. Since inception, over 36 science-based departments/units within the
+            University regularly use the facilities and the main users are members of staff, postgraduate and
+            undergraduate students for research and final year projects respectively. Apart from providing needed
+            facilities, the CSL has impacted positively on the Science disciplines through attraction of undergraduate
+            students for industrial attachment and providing assistance in meeting accreditation requirements of
+            professional regulatory bodies and those of the National Universities Commission.Over 20 other
+            universities/research institutes across the nation make use of the services of the Laboratory.
+        </p>
+        <p>
+            Essentially, the Center has been able to attract a reasonable cross section of users countrywide. The
+            CSL has also emerged as a model for other universities in Nigeria. In addition, regular hands-on training
+            workshops are held from time to time to update the knowledge of users. The CSL has been a beneficiary of
+            the Carnegie Corporation of New York support from 2003 to date for sustenance of its activities, which
+            include acquisition of some additional items of equipment and accessories, replacing aging ones, carrying
+            out maintenance services for specialized items of equipment as well as, training opportunities for CSL
+            staff members and the users.
+        </p>
+    </div>
+</div>
+<?php include ('includes/footer.inc'); ?>
+
+</body>
+</html>

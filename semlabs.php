5e2487da6f7bafca3a9fc40f8f3e907ee2e89921
hydeenoble <hydeenoble39@gmail.com>
2016-12-01 11:59:26 +0100

    worked on the nuclearlabs.php file, semlabs.php file and edited the header.inc file

---
+++ semlabs.php
@@ -0,0 +1,93 @@
+
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <title>Central Science Laboratory</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />
+    <link rel="stylesheet" href="css/style.css" type="text/css" />
+    <script  src="js/jquery.min.js"></script>
+    <script  src="js/bootstrap.min.js"></script>
+
+</head>
+<body >
+
+<?php include('includes/header.inc'); ?><br />
+
+<div class="bl-body">
+
+</div>
+<div class="about-wrapper">
+
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+            <li data-target="#myCarousel" data-slide-to="3"></li>
+            <li data-target="#myCarousel" data-slide-to="4"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+            <div class="item active">
+                <img src="images/Untitled-52.jpg" alt="first image about CSL">
+            </div>
+
+            <div class="item">
+                <img src="images/Untitled-62.jpg" alt="first image about CSL">
+            </div>
+
+            <div class="item">
+                <img src="images/Untitled-72.jpg" alt="first image about CSL">
+            </div>
+
+            <div class="item">
+                <img src="images/Untitled-82.jpg" alt="first image about CSL">
+            </div>
+
+            <div class="item">
+                <img src="images/Untitled-92.jpg" alt="first image about CSL">
+            </div>
+        </div>
+
+    </div>
+    <br><br>
+    <div class="container-fluid">
+        <h2 class="text-center">Scanning Electron Microscope Laboratory</h2>
+        <br><br>
+        <div class="row">
+            <div class="col-md-4">
+                <img class="img-responsive" src="images/person-icon.png" alt="Prof. S.A Adesanya" style="height: 200px; width: 200px; margin: auto">
+                <br>
+                <div class="container-fluid text-center">
+                    <b>Dr. Ladi Osasona</b><br>
+                    E-mail: oosasona@oauife.edu.ng <br>
+                </div>
+            </div>
+            <div class="col-md-8" style="padding: 20px">
+                <h4><b>Lab. Convener</b></h4>
+                <p>The CSL is equipped with a LEO 1450 scanning electron microscope (SEM).
+                    The LEO 1450 is a research grade conventional SEM expandable to a
+                    comprehensive imaging and a Pentium III analytical workstation with
+                    own micrograph printer (photo gloss quality). The extra-large Chamber
+                    has a high precision, fully motorized rotary stage which allows for quality
+                    analysis and view of the surface of samples. The stub-operated sample stage
+                    can handle a maximum of seven samples at once with a chamber capacity up to
+                    200mm wide and 65mm thick.
+                </p>
+                <p>A sputter coater has been installed to extend application to non-conducting
+                    samples (biological, geological, etc.). An example of scanned ceramic material
+                    is shown in this page. An X-ray detector has been acquired to expand the scope
+                    of application further. To create a versatile microscopy facility, a research
+                    microscope with digital camera and workstation has recently been installed.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include('includes/footer.inc'); ?>
+</body>
+</html>
